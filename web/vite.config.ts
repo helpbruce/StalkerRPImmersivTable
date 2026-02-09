8e4e6db6a6252d3341f91c90b16cc7d655649b4c
Bruce <egor.simagin@gmail.com>
2026-02-10 00:36:36 +0300

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,27 +1,18 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/StalkerRPImmersivTable/',   
-  plugins: [react()],
-})
-           
-export default defineConfig({
+  base: '/StalkerRPImmersivTable/',
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
